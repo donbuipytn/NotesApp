9147b090059978e341298ee8c069a47f7e9893c8
Lily Makita <lilymakita204@gmail.com>
2024-10-26 14:30:35 +0700

    Sửa lại id Layout main

--- app/src/main/java/com/example/notesapp/noteActivity.java
+++ app/src/main/java/com/example/notesapp/noteActivity.java
@@ -1,4 +1,24 @@
 package com.example.notesapp;
 
-public class noteActivity {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class noteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.act_note);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.actNote), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
